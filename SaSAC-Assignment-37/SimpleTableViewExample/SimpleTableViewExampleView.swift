a47aaf6f6551ab3bc12753946dd492bd7a622551
Minsung Kim <mskim4048@naver.com>
2025-08-19 17:47:44 +0900

    feat: SimpleTableViewExample 관련 파일 추가

---
+++ SaSAC-Assignment-37/SimpleTableViewExample/SimpleTableViewExampleView.swift
@@ -0,0 +1,12 @@
+//
+//  SimpleTableViewExampleView.swift
+//  SaSAC-Assignment-37
+//
+//  Created by 김민성 on 8/19/25.
+//
+
+import UIKit
+
+final class SimpleTableViewExampleView: UIView {
+    
+}
---
+++ SaSAC-Assignment-37/SimpleTableViewExample/SimpleTableViewExampleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTableViewExampleViewController.swift
+//  SaSAC-Assignment-37
+//
+//  Created by 김민성 on 8/19/25.
+//
+
+import UIKit
+
+class SimpleTableViewExampleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
